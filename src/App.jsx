5df68ad2a3060f8a40bd2c431e06e251039ccac6
Jedilady <jedilady@gmail.com>
2025-03-18 11:59:20 +0000

    Prueba "Hola" commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './css/App.css'
+import PruebaContext from './components/PruebaContext'
 
 function App() {
 
   return (
     <>
       <h1>HI</h1>
+      <PruebaContext/>
     </>
   )
 }

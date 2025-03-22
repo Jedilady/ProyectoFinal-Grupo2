499c38a888819e2389db738a0a21eaaaab2a7eda
Delcy <delcyacp@gmail.com>
2025-03-22 21:49:43 +0100

    componentes Poduct.list, para ver todos los productos, product.json para la carga de los productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
-import ProductCard from './ProductCard';
-import products from '../data/products'; // Importa el array de productos
+import React, { useEffect, useState } from 'react';
+import productsData from '../data/products.json'; // Importa el array de productos
 
-const ProductList = () => {
-  const [filteredProducts, setFilteredProducts] = useState(products);// Inicialmente muestra todos los productos
+// Muestra la lista de productos y maneja el estado de los productos
+function ProductList() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]); // Inicialmente muestra todos los productos
+
+  useEffect(() => {
+    setProducts(productsData); // Cargar los productos desde el archivo JSON
+    setFilteredProducts(productsData); // Inicializa los productos filtrados
+  }, []);
 
   // Función para filtrar productos por categoría
   const filterByCategory = (category) => {
@@ -17,6 +23,7 @@ const ProductList = () => {
 
   return (
     <div>
+      <h1>Lista de Productos</h1>
       <div>
         <button onClick={() => filterByCategory("Blusas")}>Blusas</button>
         <button onClick={() => filterByCategory("Pantalones")}>Pantalones</button>
@@ -25,42 +32,26 @@ const ProductList = () => {
       </div>
 
       <div className="product-list">
-        {filteredProducts.map(product => (
-          <ProductCard key={product.id} product={product} />
+        {filteredProducts.map((product) => (
+          <div key={product.id} className="product">
+            <img src={product.images.front} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>Categoría: {product.category}</p>
+            <p>Precio: ${product.price}</p>
+            <p>Tamaños disponibles: {product.sizes.join(", ")}</p>
+            <p>Colores disponibles: {product.color.join(", ")}</p>
+            <p>Stock: {product.stock}</p>
+          </div>
         ))}
       </div>
     </div>
   );
-};
-
+}
 
 export default ProductList;
-  
-  
-  
-  
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-const ProductsPage = () => {
-  return (
-    <div>
-      <h1>Lista de Productos</h1>
-      <ProductList />  {/* Aquí se debería renderizar el listado de productos */}
-    </div>
-  );
-};
-
-//export default ProductsPage;

460f597e84e2070cfa4079bf6d5d863ae071082d
Jedilady <jedilady@gmail.com>
2025-03-26 11:32:00 +0000

    Ajustes al contenido del navbar: añadido de enlaces a productos, iconos, y estilos

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,47 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { BsPersonCircle, BsCart2, BsBag } from "react-icons/bs";
 import './Navbar.css';
 
 const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-link">
-        <span className='nav-logo'>Nua</span>
-      </Link>
-
-      <Link to="/login" className="nav-link">
-        <button className="nav-button">Login</button>
-      </Link>
-
-      <Link to="/products" className="nav-link">
-        <button className="nav-button">Productos</button>
-      </Link>
-      <Link to="/cart">
-        <button className="nav-button">Carrito</button>
-      </Link>
+      <div className='nav-left'>
+        {/* Grupo de enlaces a Productos la izquierda (ocultos en móvil) */}
+        <Link to="/products/Blusas">
+            <button className="footer-button">Blusas</button>
+        </Link>
+        <Link to="/products/Pantalones">
+          <button className="footer-button">Pantalones</button>
+        </Link>
+        <Link to="/products/Vestidos">
+          <button className="footer-button">Vestidos</button>
+        </Link>
+      </div>
+      {/* Centro: logo */}
+      <div className='nav-logo-home'>
+        <Link to="/" className="nav-link">
+          <span className='nav-logo'>Nüa</span>
+        </Link>
+      </div>
+      {/* Grupo de enlaces de usuario a la derecha (iconos en móvil)*/}
+      <div className='nav-right'>
+        <Link to="/products" className="nav-link">
+          <button className="hidden-xs nav-button">Productos</button>
+          <BsBag className='hidden-md hidden-lg' />
+        </Link>
+        <Link to="/login" className="nav-link">
+          <button className="hidden-xs nav-button">Login</button>
+          <BsPersonCircle className='hidden-md hidden-lg' />
+        </Link>
+        <Link to="/cart">
+          <button className="hidden-xs nav-button">Carrito</button>
+          <BsCart2 className='hidden-md hidden-lg' />
+        </Link>
+      </div>
     </nav>
   );
 };

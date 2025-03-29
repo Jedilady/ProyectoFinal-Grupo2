a292275768aade6d5c0140b1b954776e8f7f37af
Jenn88PH <jenniferpazhernanez88@gmail.com>
2025-03-29 06:57:10 -0400

    add logout button to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
 import { useState, useContext, useEffect } from 'react';
-import { BsPersonCircle, BsCart2, BsBag, BsList, BsXLg } from "react-icons/bs";
-import { useCart } from "../../context/CartContext";
+import { BsPersonCircle, BsCart2, BsBag, BsList, BsXLg } from 'react-icons/bs';
+import { useCart } from '../../context/CartContext';
 import './Navbar.css';
 import UserContext from '../../context/UserContext';
 import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-
-  const { user } = useContext(UserContext); //obtenemos el usuario del contexto
+  const { user, logout } = useContext(UserContext); //obtenemos el usuario del contexto
   const { totalItems } = useCart(); //SE OBTIENE EL NUMERO TOTAL DE PRODUCTOS EN EL CARRITO LEIDY//
-  
+
   //Manejo del menú hamburguesa
   //seteo para manejar si el menú está abierto o cerrado (cerrado/false por default)
   const [menuOpen, setMenuOpen] = useState(false);
@@ -18,19 +17,17 @@ const Navbar = () => {
   const location = useLocation();
 
   //Usamos un useEffect para que, al cambiar de url, se cierre el menú automáticamente
-  useEffect (() => {
-      setMenuOpen(false)
-    },[location.pathname]
-  )
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location.pathname]);
   //ToDo: cerrar menu/marcar en el menú cuando se hace click al mismo url en el que se está
-
 
   //manejador de los enlaces a productos
   const navProductsContent = () => {
     return (
       <>
         <Link to="/products/Blusas">
-            <button className="footer-button">Blusas</button>
+          <button className="footer-button">Blusas</button>
         </Link>
         <Link to="/products/Pantalones">
           <button className="footer-button">Pantalones</button>
@@ -39,21 +36,18 @@ const Navbar = () => {
           <button className="footer-button">Vestidos</button>
         </Link>
       </>
-    )
-  }
+    );
+  };
 
   return (
     <nav className="navbar">
-      
       {/* Grupo de enlaces a Productos la izquierda (ocultos en móvil) */}
-      <div className='nav-left'>
-        {navProductsContent()}
-      </div>
+      <div className="nav-left">{navProductsContent()}</div>
 
       {/* Menú hamburguesa en móviles */}
-      <div className='nav-burger'>
-      <button 
-          className="nav-burger-button" 
+      <div className="nav-burger">
+        <button
+          className="nav-burger-button"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           {!menuOpen && <BsList />}
@@ -62,41 +56,51 @@ const Navbar = () => {
       </div>
 
       {/* Menú desplegable en móviles - con animación */}
-      <div className={`nav-left-mobile ${menuOpen ? "open" : ""}`}>
+      <div className={`nav-left-mobile ${menuOpen ? 'open' : ''}`}>
         {navProductsContent()}
       </div>
-      
+
       {/* Centro: logo */}
-      <div className='nav-logo-home'>
+      <div className="nav-logo-home">
         <Link to="/" className="nav-link">
-          <span className='nav-logo-img'></span>
-          <span className='nav-logo'>Nüa</span>
+          <span className="nav-logo-img"></span>
+          <span className="nav-logo">Nüa</span>
         </Link>
-      
       </div>
       {/* Grupo de enlaces de usuario a la derecha (iconos en móvil)*/}
-      <div className='nav-right'>
+      <div className="nav-right">
         {user ? (
           <>
-            <span className="nav-user hidden-xs">Hola, {user.name || 'Desconocido'}!</span>
+            <span className="nav-user hidden-xs">
+              Hola, {user.name || 'Desconocido'}!
+            </span>
             <Link to="/profile">
               <button className="hidden-xs hidden-s nav-button">Perfil</button>
-              <BsPersonCircle className='nav-icon hidden-md hidden-lg nav-user-icon-logged' />
+              <BsPersonCircle className="nav-icon hidden-md hidden-lg nav-user-icon-logged" />
             </Link>
+            <button
+              onClick={logout}
+              className="hidden-xs hidden-s nav-button logout-button"
+            >
+              Log Out
+            </button>
           </>
         ) : (
           <Link to="/login" className="nav-link">
             <button className="hidden-xs hidden-s nav-button">Login</button>
-            <BsPersonCircle className='nav-icon hidden-md hidden-lg' />
+            <BsPersonCircle className="nav-icon hidden-md hidden-lg" />
           </Link>
         )}
         <Link to="/products" className="nav-link">
           <button className="hidden-xs hidden-s nav-button">Productos</button>
-          <BsBag className='nav-icon hidden-md hidden-lg' />
+          <BsBag className="nav-icon hidden-md hidden-lg" />
         </Link>
-        <Link to="/cart" className='nav-cart-bt'>
-          <button className="hidden-xs hidden-s nav-button">Carrito ({totalItems})</button>
-          <BsCart2 className='nav-icon hidden-md hidden-lg' /><span className='hidden-md hidden-lg'>({totalItems})</span>
+        <Link to="/cart" className="nav-cart-bt">
+          <button className="hidden-xs hidden-s nav-button">
+            Carrito ({totalItems})
+          </button>
+          <BsCart2 className="nav-icon hidden-md hidden-lg" />
+          <span className="hidden-md hidden-lg">({totalItems})</span>
         </Link>
       </div>
     </nav>
